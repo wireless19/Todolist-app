675ed5f0d80b55c2efeed71dfcbf36192af55b1c
victor <victorifeanyichukwu4@gmail.com>
2021-05-01 10:10:35 +0100

    modified some css

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 app.use(express.static("public"));
 
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
